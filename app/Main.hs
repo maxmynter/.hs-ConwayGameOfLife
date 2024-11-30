bd35fdddda90ab58fa8fb51334dc38f26af61170
Max Mynter <maxmynter@me.com>
2024-11-30 02:01:24 +0100

    Print to COnsole

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
-module Main (main) where
+module Main where
 
-import Lib
+type Grid = [[Bool]]
+
+initialGrid :: Int -> Int -> Grid
+initialGrid width height = replicate height (replicate width False)
+
+printGrid :: Grid -> IO ()
+printGrid =
+  mapM_ (putStrLn . map (\cell -> if cell then '█' else ' '))
 
 main :: IO ()
-main = someFunc
+main = do
+  let grid = initialGrid 20 20
+  printGrid grid
